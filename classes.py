83ef8a542d4c78c687aec62d1ae85edb3c4977cf
William Salas <willbullet52@gmail.com>
2023-05-04 13:29:55 -0500

    student class

--- classes.py
+++ classes.py
@@ -20,6 +20,32 @@ class Car(Automobile):
             f'this car can go up to {self.speed}km/h and has {self.cilinders} cilinders')
 
 
-car = Car(8, 6, 'white', 4, 3)
+""" car = Car(8, 6, 'white', 4, 3)
 car._getCar()
-car._getSpecs()
+car._getSpecs() """
+
+
+
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def _getInfo(self):
+        print(f'the student name is {self.name} and grade is {self.grade}')
+
+    def _didApprove(self):
+        if self.grade >= 3:
+            return print(f'the student {self.name} has approved with grade {self.grade}')
+        print(f'the student {self.name} has not approved with grade {self.grade}')
+
+
+jake = Student('Jake', 4)
+carl = Student('Carl', 2.9)
+
+
+jake._getInfo()
+jake._didApprove()
+
+carl._getInfo()
+carl._didApprove()
